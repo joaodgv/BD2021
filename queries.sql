2b05ecd45c1e0ff7928402eb1c653f34c74805d3
joaodgv <joaodiogo.veloso@gmail.com>
2020-11-16 16:38:06 +0000

    creating the html files

--- queries.sql
+++ queries.sql
@@ -2,31 +2,31 @@ SELECT num_concelho
 FROM concelho
 
 
-concelho onde se fez o maior volume de vendas hoje
+--concelho onde se fez o maior volume de vendas hoje
 
 
-selecionar o concelho com mais vendas de todos os conselhos
-para cada conselho, somar as vendas de todas as suas instituicoes
-para cada instituicao, somar as vendas todas dela 
-
-
-
-medico que mais prescreveu no 1º semestre de 2019 em cada região
-
-selecionar para cada regiao 
-o medico com mais prescricoes da tabela prescricoes que sejam da mesma regiao 
+--selecionar o concelho com mais vendas de todos os conselhos
+--para cada conselho, somar as vendas de todas as suas instituicoes
+--para cada instituicao, somar as vendas todas dela 
 
 
 
-os medicos que ja prescreveram aspirina em receitas aviadas em todas as farmacias de arouca deste ano
+--medico que mais prescreveu no 1º semestre de 2019 em cada região
 
-selecionar num_cedula
-da tabela prescricao_venda (estas filtradas para serem apenas de Arouca)
-onde substancia=aspirina e ano=current_year
-
+--selecionar para cada regiao 
+--o medico com mais prescricoes da tabela prescricoes que sejam da mesma regiao 
 
 
-doentes que ja fizerqam analises ,mas ainda nao aviaram prescriçoes este mes 
 
-selecionar num_doente
-da tabela consulta ^ analise ^ NOT(selecionar num_doente da tab prescricao_venda) 
+--os medicos que ja prescreveram aspirina em receitas aviadas em todas as farmacias de arouca deste ano
+
+--selecionar num_cedula
+--da tabela prescricao_venda (estas filtradas para serem apenas de Arouca)
+--onde substancia=aspirina e ano=current_year
+
+
+
+--doentes que ja fizerqam analises ,mas ainda nao aviaram prescriçoes este mes 
+
+--selecionar num_doente
+--da tabela consulta ^ analise ^ NOT(selecionar num_doente da tab prescricao_venda) 
--- web/server.py
+++ web/server.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello miguel'
+def index(name=None):
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+   app.run()
