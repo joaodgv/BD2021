cecab6265747a8025f55aa8a6fb2a4777900fab4
AngolaENossa <rafaelafcmcandeias@gmail.com>
2020-12-04 16:43:20 +0000

    feito

--- triggers.sql
+++ triggers.sql
@@ -1,27 +1,39 @@
--- Trigger 1
+-- RI - 100	
 -- DROP TRIGGER IF EXISTS verifica_especialidade_trigger ON analise;
-CREATE OR REPLACE FUNCTION verify_nconsultas() TRIGGER AS $$
+CREATE OR REPLACE FUNCTION verifica_nconsultas() RETURNS TRIGGER AS $$
 BEGIN
-	SELECT DATEPART(week, data) AS week, consulta.nome_instituicao
-	FROM consulta
-	WHERE MIN(consulta.data_consulta) <= data <= MAX(consulta.data_consulta)
-	GROUP BY DATEPART(week, data);
+	IF COUNT(t.Week) = 100
+		FROM (
+			SELECT WEEK(consulta.data_consulta) AS Week, YEAR(consulta.data_consulta) AS Year
+			FROM consulta
+			WHERE consulta.num_cedula = new.num_cedula AND consulta.nome_instituicao = new.nome_instituicao
+				AND YEAR(consulta.data_consulta) = YEAR(new.data_consulta) AND WEEK(consulta.data_consulta) = WEEK(new.data_consulta)
+			)
+		AS t;
+	THEN
+		RAISE EXCEPTION 'Médico % já deu 100 consultas nesta semana de % na instituicao %', new.num_cedula, new.data_consulta, new.nome_instituicao;
+	END IF;
+	RETURN new;
+
 END;
 $$ Language plpgsql;
-CREATE TRIGGER verifica_especialidade_trigger BEFORE INSERT ON consulta
-FOR EACH ROW EXECUTE PROCEDURE verify_nconsultas();
+CREATE TRIGGER verifica_NCONSULTAS_trigger BEFORE INSERT ON consulta
+FOR EACH ROW EXECUTE PROCEDURE verifica_nconsultas();
 
--- Trigger 2
+
+-- RI - analise
 -- DROP TRIGGER IF EXISTS verifica_especialidade_trigger ON analise;
 CREATE OR REPLACE FUNCTION verifica_especialidade() RETURNS TRIGGER AS $$
 BEGIN
-	IF NEW.num_cedula <> null AND NEW.num_doente <> null AND NEW.data_consulta <> null AND NEW.num_cedula <> (
+	IF new.num_cedula <> null AND new.num_doente <> null AND new.data_consulta <> null AND new.num_cedula <> (
 		SELECT medico.especialidade
 		FROM medico
-		WHERE medico.num_cedula = NEW.num_cedula
+		WHERE medico.num_cedula = new.num_cedula
 	) THEN
-		RAISE EXCEPTION 'Análise % não tem especialidade correta', NEW.num_analise;
+		RAISE EXCEPTION 'Análise % não tem especialidade correta', new.num_analise;
 	END IF;
+	RETURN new;
+
 END;
 $$ Language plpgsql;
 CREATE TRIGGER verifica_especialidade_trigger BEFORE INSERT ON analise
